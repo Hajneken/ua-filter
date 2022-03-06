4f0754f21e286ee6d4a88d572d358e335f619d23
Hynek <hajneken@towelDispenser.localdomain>
2022-03-06 22:09:46 +0100

    additional gitignore rules

---
+++ src/components/H1/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+    background: -webkit-linear-gradient(top, #695901 0 25%, #ffd700);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  filter: drop-shadow(2px 4px 8px #00000026);
+`
+
+export { H1 }
